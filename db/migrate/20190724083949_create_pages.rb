81b7e3650d8cf6e60b2cd27cb4b12a244ead96d0
geetha502 <geethaa502@gmail.com>
2019-07-24 16:59:50 +0530

    simple_cms application with database

---
+++ db/migrate/20190724083949_create_pages.rb
@@ -0,0 +1,14 @@
+class CreatePages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pages do |t|
+
+      t.references 'subject'
+      t.string 'name'
+      t.string 'permalink'
+      t.integer 'position'
+      t.boolean 'visible'
+
+      t.timestamps
+    end
+  end
+end
